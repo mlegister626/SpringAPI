ebbc5dc48f968c4c0a44baf4f5989e7ff6339789
Marcello Legiser <mlegister626@gmail.com>
2025-05-15 13:27:14 -0400

    Adding Hello World so I can test git/github commits

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -8,5 +8,6 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
+        System.out.println("Hello World");
     }
 }

1f514acafb541d30caf49ac82f13914bee096762
Marcello Legiser <mlegister626@gmail.com>
2025-05-16 15:00:47 -0400

    Finished the First Section, Ready to commit and learn Vim :p

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -9,5 +9,6 @@ public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
         System.out.println("Hello World, its time to make a store using Spring to make APIs.");
+
     }
 }
---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+}
